ef93f3b7e88bdb68bf8f40b81acdf4262713f54d
maria-martinezr <maria.martinezr@perficient.com>
2022-07-29 15:29:34 -0500

    Refactor tests with AAA patern

--- cypress/page/menu-content.page.ts
+++ cypress/page/menu-content.page.ts
@@ -1,9 +1,12 @@
+
 class MenuContentPage {
     private tShirtMenu: string;
+    private dressesMenu: string;
     private menuContentPageURL: string;
 
     constructor() {
         this.tShirtMenu = "#block_top_menu > ul > li > a[title='T-shirts']";
+        this.dressesMenu = "#block_top_menu > ul > li > a[title='Dresses']";
         this.menuContentPageURL = "http://automationpractice.com/";
     }
 
@@ -13,6 +16,10 @@ class MenuContentPage {
 
     public goToTShirtMenu(): void {
         cy.get(this.tShirtMenu).click();
+    }
+
+    public goToDressesMenu(): void {
+        cy.get(this.dressesMenu).click();
     }
 }
 
