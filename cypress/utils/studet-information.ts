010d84467623ea2704a41589bccf5be597e1a073
maria-martinezr <101816784+maria-martinezr@users.noreply.github.com>
2022-08-03 09:31:01 -0500

    add E2E to fill in a form (#13)

---
+++ cypress/utils/studet-information.ts
@@ -0,0 +1,14 @@
+interface StudentInformation {
+    name: string;
+    lastName: string;
+    email: string;
+    gender: string;
+    dateOfBirth: string;
+    mobileNumber: string;
+    hobbies: string[],
+    currentAddress: string;
+    state: string;
+    city: string;
+  };
+
+  export { StudentInformation }
